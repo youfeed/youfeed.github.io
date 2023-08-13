19b96b0959a69861edec176711cf8e11ba4b2b31
youfeed <11247005@qq.com>
2023-08-13 20:23:02 +0800

    0.0.1

---
+++ .vitepress/config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitepress'
+export default defineConfig({
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
